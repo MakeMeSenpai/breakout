aeaa15d8aa1d1b58ec15977c830b48351c46ace8
Anthony <sean.a.protho1@gmail.com>
2020-04-02 11:03:31 -0700

    eslint

---
+++ index.js
@@ -0,0 +1,14 @@
+/* Hello World! using eslint.
+ this is just tester code for lint
+ but this is where our breakout code
+ will be! */
+
+const hello = "hello";
+const goodbye = "word";
+
+function star() {
+  const world = `${hello} ${goodbye}`;
+  return world;
+}
+
+star();
