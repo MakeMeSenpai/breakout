af66f22150187f2ededcda4b3de69c9575ff455f
Anthony <sean.a.protho1@gmail.com>
2020-04-09 11:11:02 -0700

    clear and draw func

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ let y = canvas.height - 30;
 const dx = 2;
 const dy = -2;
 
-function draw() {
+function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, 10, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
   x += dx;
   y += dy;
 }
